beaa245d8bdc05fa727eddddd907582633acb7cb
iceFreya <3589507161@qq.com>
2026-04-02 14:27:25 +0800

    Add files via upload

---
+++ cotry1.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+DEEPSEEK_API_KEY = "sk-"
+
+API_URL = "https://api.deepseek.com/v1/chat/completions"
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {DEEPSEEK_API_KEY}"
+}
+
+prompt = "一个农场里有鸡和兔子，头的总数是 35，脚的总数是 94。鸡和兔子各有多少只？"
+
+request_data = {
+    "model":"deepseek-chat",
+    "messages":[
+        {"role":"user","content":prompt}
+    ]
+}
+
+try:
+    response = requests.post(API_URL, json=request_data, headers=headers)
+    response.raise_for_status()
+
+    result = response.json()
+    answer = result["choices"][0]["message"]["content"]
+    print("======answer======")
+    print(answer)
+
+except requests.exceptions.RequestException as e:
+    print(e)
+    if hasattr(e,'response') and e.response is not None:
+        print(f"wrong!{e.response}")
+except KeyError as e:
+    print(e)
+
